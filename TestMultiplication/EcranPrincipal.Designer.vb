3f979cfa701e5f7846a5dafdd6c5d4e4eee1081a
Sébastien RAILLARD <sr@coexsi.fr>
2020-04-03 14:29:57 +0200

    premiere version

---
+++ TestMultiplication/EcranPrincipal.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EcranPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lQuestion = New System.Windows.Forms.Label()
+        Me.tbReponse = New System.Windows.Forms.TextBox()
+        Me.lInfos = New System.Windows.Forms.Label()
+        Me.tTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'lQuestion
+        '
+        Me.lQuestion.Font = New System.Drawing.Font("Consolas", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lQuestion.Location = New System.Drawing.Point(24, 23)
+        Me.lQuestion.Name = "lQuestion"
+        Me.lQuestion.Size = New System.Drawing.Size(258, 61)
+        Me.lQuestion.TabIndex = 0
+        Me.lQuestion.Text = "99 x 99 ="
+        Me.lQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'tbReponse
+        '
+        Me.tbReponse.Font = New System.Drawing.Font("Consolas", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbReponse.Location = New System.Drawing.Point(288, 20)
+        Me.tbReponse.Name = "tbReponse"
+        Me.tbReponse.Size = New System.Drawing.Size(100, 64)
+        Me.tbReponse.TabIndex = 1
+        Me.tbReponse.Text = "999"
+        Me.tbReponse.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lInfos
+        '
+        Me.lInfos.Location = New System.Drawing.Point(12, 103)
+        Me.lInfos.Name = "lInfos"
+        Me.lInfos.Size = New System.Drawing.Size(412, 22)
+        Me.lInfos.TabIndex = 2
+        Me.lInfos.Text = "Infos"
+        Me.lInfos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'tTimer
+        '
+        Me.tTimer.Enabled = True
+        '
+        'EcranPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(436, 134)
+        Me.Controls.Add(Me.lInfos)
+        Me.Controls.Add(Me.tbReponse)
+        Me.Controls.Add(Me.lQuestion)
+        Me.Name = "EcranPrincipal"
+        Me.Text = "Test Multiplication"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lQuestion As Windows.Forms.Label
+    Friend WithEvents tbReponse As Windows.Forms.TextBox
+    Friend WithEvents lInfos As Windows.Forms.Label
+    Friend WithEvents tTimer As Windows.Forms.Timer
+End Class
---
+++ TestMultiplication/EcranPrincipal.vb
@@ -0,0 +1,111 @@
+﻿Imports System.Windows.Forms
+
+Public Class EcranPrincipal
+
+    ' Stockage liste
+    Public Liste(0 To 65) As String
+    ' Random
+    Public r As Random
+    ' Index question
+    Public Index As Integer = 0
+    ' Question
+    Public a, b As Integer
+    ' Erreurs
+    Public NbErreurs As Integer = 0
+    ' Temps total
+    Public TempsTotal As Integer = 0
+    ' Mesure temps
+    Public tsDebut, tsFin As DateTime
+
+    Private Sub EcranPrincipal_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        ' Création liste
+        Dim x, y, n As Integer
+        n = 0
+        For x = 0 To 10
+            For y = 0 To 10
+                If y <= x Then
+                    Liste(n) = x.ToString + "," + y.ToString
+                    n +=1
+                End If
+            Next
+        Next
+
+        ' Random liste
+        r = New Random(My.Computer.Clock.TickCount)
+        Dim i, j As Integer
+        For i = 0 To Liste.Count - 1
+            j = r.Next(i, Liste.Count - 1)
+            Dim s As String = Liste(i)
+            Liste(i) = Liste(j)
+            Liste(j) = s
+        Next
+
+        ' Message démarrage
+        MessageBox.Show("Cliquez sur OK pour commencer !", "Démarrage", MessageBoxButtons.OK)
+
+        ' Première question
+        Index = 0
+        NbErreurs = 0
+        TempsTotal = 0
+        AffichageQuestion()
+
+    End Sub
+
+    Public Sub AffichageQuestion()
+        ' Préparation question
+        Dim s As String = Liste(Index)
+        Dim Tokens() As String = s.Split(","c)
+        If r.Next(1, 2) = 2 Then
+            a = CInt(Tokens(0))
+            b = CInt(Tokens(1))
+        Else
+            a = CInt(Tokens(1))
+            b = CInt(Tokens(0))
+        End If
+        ' Affichage
+        lQuestion.Text = a.ToString + " x " + b.ToString + " = "
+        tbReponse.Text = ""
+        tbReponse.Focus()
+        ' Question suivante
+        Index += 1
+        ' Début réponse
+        tsDebut = DateTime.Now
+    End Sub
+
+    Private Sub tbReponse_KeyDown(sender As Object, e As KeyEventArgs) Handles tbReponse.KeyDown
+        If e.KeyCode = Keys.Enter And tbReponse.Text <> "" Then
+            ' Récupération réponse
+            Dim Reponse As Integer
+            Try
+                Reponse = CInt(tbReponse.Text)
+            Catch ex As Exception
+                Reponse = -1
+            End Try
+            ' Fin réponse
+            tsFin = DateTime.Now
+            ' Calcul durée
+            TempsTotal += tsFin.Subtract(tsDebut).TotalSeconds
+            ' Test résultat
+            If a * b <> Reponse Then
+                ' Erreur !
+                NbErreurs += 1
+                MessageBox.Show(a.ToString + "x" + b.ToString + "=" + (a * b).ToString + " et non pas " + Reponse.ToString + " !" + vbCrLf + "Cliquez sur OK pour continuer !", "Erreur", MessageBoxButtons.OK)
+            End If
+            ' Question suivante ou fin
+            If Index <= Liste.Count - 1 Then
+                ' Question suivante
+                AffichageQuestion()
+            Else
+                ' Fin
+                MessageBox.Show("Le test est fini !", "Fin", MessageBoxButtons.OK)
+            End If
+        End If
+    End Sub
+
+
+    Private Sub tTimer_Tick(sender As Object, e As EventArgs) Handles tTimer.Tick
+        lInfos.Text = "Question " + Index.ToString + " sur " + Liste.Count.ToString + " - " + NbErreurs.ToString + " erreurs - Temps total de " + TempsTotal.ToString + " secondes"
+    End Sub
+
+End Class
